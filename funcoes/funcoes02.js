cf3cf2068758461e6ffe70322af19ae9fd3fccfe
profLedson <ledson.vanini@escola.pr.gov.br>
2024-10-25 19:36:18 +0000

    Criar função nomeada e anônima

---
+++ funcoes/funcoes02.js
@@ -0,0 +1,20 @@
+
+// FUNÇÕES NOMEADAS
+function logar(usuario, senha) {
+    alert('Logado com a função nomeada!')
+    alert('Olá, ' + usuario)
+}
+logar("ledson123", 12345)
+
+
+
+// FUNÇÕES ANÔNIMAS
+const conectar = function(usuario, senha){
+    alert('Logado com a função anônima!')
+    alert('Olá, ' + usuario)
+}
+
+
+
+// ARROW FUNCTIONS OU FUNÇÕES FLECHA
+
