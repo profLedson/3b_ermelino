1d9238dd21a678ffe31915f7adca9e80c016de57
profLedson <ledson.vanini@escola.pr.gov.br>
2024-11-01 18:20:18 +0000

    Clear Interval e Set Inverval do Javascript

--- funcoes/setTimeout.js
+++ funcoes/setTimeout.js
@@ -3,3 +3,16 @@ setTimeout(function(){
     console.log("Tempo esgotado!")
 }, 4000)
 
+// setInterval (função, tempo ms)
+let fps = Math.round( Math.random() * 30)
+
+const intervalo1 = setInterval(function() {
+    console.clear() // LIMPAR A TELA
+    console.log("Fps: " + fps)
+    fps = fps + 1
+}, 1000)
+
+setTimeout(function(){
+    clearInterval(intervalo1)
+    console.log("Conexão encerrada!")
+}, 5000)
